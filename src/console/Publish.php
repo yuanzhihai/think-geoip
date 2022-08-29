9c8cd7cb536553c57a4d6b7aa8e910c8de48f9bd
yuanzhihai <396751927@qq.com>
2022-08-29 12:00:02 +0800

    init

--- src/console/Publish.php
+++ src/console/Publish.php
@@ -18,9 +18,34 @@ class Publish extends Command
     public function execute(Input $input, Output $output)
     {
         //获取默认配置文件
-        $resources = __DIR__ . '/../../../think-geoip/resources';
-        copy($resources, base_path());
+        $resources = __DIR__ . '/../../resources';
+        $this->xCopy($resources, root_path('resources'));
         $output->writeln('publish ok');
+    }
+
+    public function xCopy($source, $destination, $child = 1)
+    {
+        if (!is_dir($source)) {
+            echo("Error:the $source is not a direction!");
+            return 0;
+        }
+        if (!is_dir($destination)) {
+            if (!mkdir($destination, 0777) && !is_dir($destination)) {
+                throw new \RuntimeException(sprintf('Directory "%s" was not created', $destination));
+            }
+        }
+        $handle = dir($source);
+        while ($entry = $handle->read()) {
+            if (($entry !== ".") && ($entry !== "..")) {
+                if (is_dir($source . "/" . $entry)) {
+                    if ($child) {
+                        $this->xCopy($source . "/" . $entry, $destination . "/" . $entry, $child);
+                    }
+                } else {
+                    copy($source . "/" . $entry, $destination . "/" . $entry);
+                }
+            }
+        }
     }
 
 }
